a784ccc76467a084b2497fa3eb7a3099f084f819
Emile Bondzie-Arthur <frederickauthur@hotmail.com>
2020-12-16 12:17:16 +0000

    some modifications

--- sample_test.py
+++ sample_test.py
@@ -1,38 +1,63 @@
-# linear search function 
+# linear search function
 from typing import List
-def linear_search(arr:List, x:int) -> int:
+from math import pi
+from collections.abc import Iterable
+
+
+def linear_search(arr: List, x: int) -> int:
     """
     Time complexity of O(n)
     """
     for i in range(len(arr)):
         if arr[i] == x:
-            return i 
-    return -1 
+            return i
+    return -1
 
 
-def binary_search(arr:List, x:int):
+def binary_search(arr: List, x: int):
     low = 0
-    mid = 0 
-    high = len(arr)-1 
+    mid = 0
+    high = len(arr)-1
 
     while low <= high:
         mid = (high + low) // 2
 
         if arr[mid] == x:
-             return mid 
+            return mid
 
-        elif arr[mid] < x :
-            low = mid + 1 
-        
+        elif arr[mid] < x:
+            low = mid + 1
+
         else:
-            high = mid - 1 
-    
+            high = mid - 1
+
     return -1
 
+
+def double_pi(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('x must be numeric')
+    elif x < 0:
+        raise ValueError('x cannot be negative')
+    else:
+        return x*pi
+
+
+def sum(values):
+    if not isinstance(values, Iterable):
+        raise TypeError('pararmeter must be an Iterable type')
+
+    total = 0
+    for v in values:
+        if not isinstance(v, (int, float)):
+            raise TypeError('elements must be numeric')
+        total += v
+    return total
+
+
 if __name__ == "__main__":
-    arr = [1,2,4,10,5,8]
-    
-    # call function 
+    arr = [1, 2, 4, 10, 5, 8]
+    # call function
     if binary_search(arr, 4) == 2:
         print("Passed Test")
     else:
--- time_complexity.py
+++ time_complexity.py
@@ -1,26 +1,27 @@
-# swaping two numbers in a list 
+# swaping two numbers in a list
 def swap(lst, a, b):
-    lst[a], lst[b] = lst[b], lst[a] 
+    lst[a], lst[b] = lst[b], lst[a]
 
 
-# finding the partitioning point within the array 
-def bleh(A, p, a):
-    r = A[q] 
-    i = p-1 
-    for j in range(p,q):
+# finding the partitioning point within the array
+def bleh(A, p, q):
+    r = A[q]
+    i = p-1
+    for j in range(p, q):
         if (A[j] < r):
             i += 1
-            swap(A, i, j) 
+            swap(A, i, j)
     if (A[q] < A[i+1]):
         swap(A, i+1, q)
-    return i+1 
+    return i+1
+
 
 def quuz(A, x, y):
     if (x < y):
-        z = bleh(A, x, y) 
-        quuz(A, x, z-1) 
+        z = bleh(A, x, y)
+        quuz(A, x, z-1)
         if (z < y):
-            quuz(A, z+1, y) 
+            quuz(A, z+1, y)
 
 
 def corge(A, n):
