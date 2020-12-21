a00d26e8267b6efa7ab5c7812ffce06c09bdbed3
Emile Bondzie-Arthur <frederickauthur@hotmail.com>
2020-12-21 01:11:41 +0000

    added extended creditcard class

--- creditcard.py
+++ creditcard.py
@@ -3,9 +3,13 @@ class CreditCard:
 
     def __init__(self, customer, bank, acnt, limit):
         """ Create a new credit card instance.
-        The initial balance is zerio.
+        The initial balance is zero.
 
-        customer the name """
+        customer    the customer the name (eg. 'Emile Mensah')
+        bank        the name of the bank (eg. 'Accra Bank')
+        acnt        the account identifier (eg. '532 552 353')
+        limit       the credit limit (measured in usd)
+        """
         self._customer = customer
         self._bank = bank
         self._account = acnt
@@ -48,6 +52,40 @@ class CreditCard:
     def make_payment(self, amount):
         """Process customer payment that reduces balance"""
         self._balance -= amount
+
+
+# class extension (Inheritance)
+class PredatoryCreditCard(CreditCard):
+    """An extension to CreditCard that compounds interest and fees"""
+
+    def __init__(self, customer, bank, acnt, limit, apr):
+        """ Create anew predatory credit card instance.
+        The initial balance is zero.
+
+        customer    the customer the name (eg. 'Emile Mensah')
+        bank        the name of the bank (eg. 'Accra Bank')
+        acnt        the account identifier (eg. '532 552 353')
+        limit       the credit limit (measured in usd)
+        """
+        super().__init__(customer, bank, acnt, limit)  # call super constructor
+        self._apr = apr
+
+    def charge(self, price):
+        """ Charge given price to the card, assuming sufficient credit limit.
+        Return True if charge was processed.
+        Return False and asses $5 if charge is denied.
+        """
+        success = super().charge(price)  # call inherited method
+        if not success:
+            self._balance += 5  # asses penalty
+        return success          # caller expects return value
+
+    def process_month(self):
+        """Asses monthly interest on outstanding balance."""
+        if self._balance > 0:
+            # if postive balance, convert APR to monthly multiplicative factor
+            monthly_factor = pow(1+self._apr, 1/12)
+            self._balance *= monthly_factor
 
 # Testing the class
 
